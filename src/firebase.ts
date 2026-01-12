2e7ac2b8bf05def83ad6d6ff3280fc0ec8228b39
misk0zz <misk0z@proton.me>
2026-01-12 01:07:00 +0100

    Initial commit: Mi portfolio v1.03

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAKBrOeQPXYwkicBRjSQIRF9M7QPTf1v_o",
+  authDomain: "portfolio-cc403.firebaseapp.com",
+  projectId: "portfolio-cc403",
+  storageBucket: "portfolio-cc403.firebasestorage.app",
+  messagingSenderId: "963239524620",
+  appId: "1:963239524620:web:0e81cf9724b374af01b129",
+  measurementId: "G-LTV50176TL"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
